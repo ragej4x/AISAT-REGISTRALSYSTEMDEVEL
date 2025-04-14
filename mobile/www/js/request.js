191d2e08b3c5c54ca69574dd8a4faa748be413fb
RAGEJAX <109853879+ragej4x@users.noreply.github.com>
2025-04-15 05:31:22 +0800

    Update request.js

--- mobile/www/js/request.js
+++ mobile/www/js/request.js
@@ -1,16 +1,14 @@
 
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();  // Prevent the form from submitting normally
+    event.preventDefault(); 
 
-    // Get form data
     const username = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    // Prepare the data to send
+
     const data = { username, email, password };
 
-    // Send the data to the server via fetch
     fetch('/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -20,7 +18,6 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     .then(data => {
         if (data.message) {
             alert('Login successful!');
-            // Redirect or perform other actions after successful login
         } else {
             alert('Login failed: ' + data.error);
         }
